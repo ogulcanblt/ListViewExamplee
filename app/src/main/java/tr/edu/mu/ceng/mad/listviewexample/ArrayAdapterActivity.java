e868f3e905c97c9cfeafd15d884a09809f3ddcba
ogulcanblt <91551180+ogulcanblt@users.noreply.github.com>
2022-10-02 13:40:09 +0300

    Initial commit

---
+++ app/src/main/java/tr/edu/mu/ceng/mad/listviewexample/ArrayAdapterActivity.java
@@ -0,0 +1,28 @@
+package tr.edu.mu.ceng.mad.listviewexample;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ArrayAdapterActivity extends ListActivity {
+
+    static final String[] ANIMALS = new String[] {"Ant", "Cat", "Dog", "Monkey"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<>(this, R.layout.activity_array_adapter, ANIMALS));
+
+        getListView().setTextFilterEnabled(true);
+
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ArrayAdapterActivity.this, ((TextView) view).getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
